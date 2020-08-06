56b3f5b4b06076fbfb664a63de5c52eef03bb87b
Fayçal BORDJAH <fbordjah@linagora.com>
2020-08-06 16:06:42 +0200

    Add secrets

--- backend/src/Controller/DefaultController.php
+++ backend/src/Controller/DefaultController.php
@@ -8,11 +8,6 @@ use Doctrine\DBAL\Driver\Connection;
 
 class DefaultController
 {
-    private const FAKEDATA = [
-        ["id"=>1,"title"=>"SYMFONYYYY","completed"=>false],
-        ["id"=>2,"title"=>"Add a task","completed"=>false],
-        ["id"=>3,"title"=>"Add a task","completed"=>false]
-    ];
     public function index (Connection $connection){
         $todos = $connection->fetchAll('SELECT * FROM todo');
 
