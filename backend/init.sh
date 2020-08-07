3b16e4514866b2486c0a9a57a1a8e5272cb7c446
Fayçal BORDJAH <fbordjah@linagora.com>
2020-08-07 15:12:37 +0200

    add

--- backend/init.sh
+++ backend/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e 
-php /var/www/html/bin/console doctrine:migrations:migrate --force
+php /var/www/html/bin/console doctrine:migrations:migrate --all-or-nothing
 eval "/usr/local/bin/docker-php-entrypoint $@"
 
